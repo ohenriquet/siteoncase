4dbdaaba9e2609a27efa2bb3cd8ca2bb5f509ecc
ohenriquet <ohenriquet@gmail.com>
2021-05-31 12:11:38 -0300

    Redirecionamentos de endereços antigos

--- next.config.js
+++ next.config.js
@@ -33,18 +33,46 @@ module.exports = withPlugins([
         exportPathMap: function () {
             return {
                 '/': { page: '/' },
+                '/solucoes': { page: '/' },
+                '/servicos': { page: '/' },
+                '/cases-e-aplicacoes': { page: '/' },
+                '/contact': { page: '/contato' },
                 '/contato': { page: '/contato' },
+                '/about-us': { page: '/sobre-nos' },
                 '/sobre-nos': { page: '/sobre-nos' },
+                '/solucoes/big-data': { page: '/big-data' },
                 '/big-data': { page: '/big-data' },
+                '/produtos': { page: '/scora-journey' },
                 '/scora-journey': { page: '/scora-journey' },
                 '/inteligencia-artificial': { page: '/inteligencia-artificial' },
+                '/solucoes/inteligencia-artificial': { page: '/inteligencia-artificial' },
+                '/solucoes/business-intelligence-analytics': { page: '/oncase-labs' },
+                '/solucoes/oncase-labs': { page: '/oncase-labs' },
                 '/oncase-labs': { page: '/oncase-labs' },
+                '/tecnologias/cloudera': { page: '/cloudera' },
+                '/tecnologias/mongodb': { page: '/mongodb' },
+                '/tecnologias/pentaho': { page: '/pentaho' },
                 '/cloudera': { page: '/cloudera' },
                 '/mongodb': { page: '/mongodb' },
-                '/pentaho': { page: '/pentaho' }
+                '/pentaho': { page: '/pentaho' },
+                '/inteligencia-artificial-vendas': { page: '/inteligencia-artificial-vendas' },
+                '/o-que-e-etl': { page: '/o-que-e-etl' },
+                '/mapa-jornada-cliente': { page: '/mapa-jornada-cliente' }
+
             };
-        }
+        },
+        experimental: {
+            redirects() {
+              return [
+                {source: '/about',
+                destination: '/sobre-nos',
+                permanent: true}
+              ]
+            }
+          }
 
     }],
-    [withCSS], [withFonts], [withSass]
+    [withCSS], 
+    [withFonts], 
+    [withSass]
 ]);
